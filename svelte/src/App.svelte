b851e22249e888fda947101342867956b56adf4e
Aaron Stanek <amr.stanek@gmail.com>
2022-08-19 11:03:03 -0700

    created header

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	export let name: string;
+	import Header1 from "./Header1.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="centraltext">
+	<Header1 tag="quick-links" text="Quick Links"></Header1>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.centraltext {
+		border-style: solid;
+		border-width: 1px;
+		border-color: black;
+		border-radius: 30px;
+		margin-top: 10px;
+		margin-bottom: 10px;
+		margin-left: auto;
+		margin-right: auto;
+		width: 61.8%;
+		padding: 20px;
 	}
 </style>
---
+++ svelte/src/Header1.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let tag: string;
+    export let text: string;
+</script>
+
+<h1 id={tag}>{text}</h1>
+
+<hr>
+
+<style>
+    hr {
+        border: 0px;
+        height: 0.5px;
+        background-color: black;
+    }
+</style>
