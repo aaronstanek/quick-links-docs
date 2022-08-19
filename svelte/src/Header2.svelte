641303ca7015604be1ff5b82196645059841e051
Aaron Stanek <amr.stanek@gmail.com>
2022-08-19 11:07:28 -0700

    added header 2 and header 3

---
+++ svelte/src/Header2.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let tag: string;
+    export let text: string;
+</script>
+
+<h2 id={tag}>{text}</h2>
+
+<style>
+</style>
---
+++ svelte/src/Header3.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let tag: string;
+    export let text: string;
+</script>
+
+<h3 id={tag}>{text}</h3>
+
+<style>
+</style>
